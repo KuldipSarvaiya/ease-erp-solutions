665a7c108e1e8e17e04a677d6e96a9afe88df5d8
Kuldip Sarvaiya <kuldipsarvaiya94@gmail.com>
2024-04-06 16:18:03 +0530

    mongoose.model -> mongoose.models

---
+++ src/app/api/hr/department/route.js
@@ -0,0 +1,10 @@
+import Department from "@/lib/models/department.model.js";
+import connectDB from "@/lib/mongoose";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  await connectDB();
+  const depts = await Department.find({}).select("dept_name _id");
+  console.log(depts);
+  return NextResponse.json(depts);
+}
