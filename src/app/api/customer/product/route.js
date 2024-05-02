fc1398be550f09b1d3a34ec5565dab174d152b50
Kuldip Sarvaiya <kuldipsarvaiya94@gmail.com>
2024-05-02 15:49:10 +0530

    products search and sort done in customer/products

--- src/app/api/customer/product/route.js
+++ src/app/api/customer/product/route.js
@@ -3,9 +3,20 @@ import connectDB from "@/lib/mongoose";
 import { NextResponse } from "next/server";
 
 export async function GET(request) {
+  const search_string =
+    new URL(request.url)?.searchParams?.get("search_string")?.split("+") || "";
+
+  const qry = { find: {} };
+  if (search_string)
+    qry.find = {
+      tags: {
+        $all: search_string.map((str) => new RegExp(`^.*${str}.*$`, "i")),
+      },
+    };
+
   await connectDB();
 
-  const res = await Product.find({})
+  const res = await Product.find(qry.find)
     .sort({ available_stock_units: -1 })
     .limit(20);
 
--- src/app/customer/products/page.js
+++ src/app/customer/products/page.js
@@ -8,27 +8,60 @@ import Loading from "@/components/Loading";
 
 function ProductsPage() {
   const [products, setProducts] = useState([]);
+  const [store, setStore] = useState([]);
+
+  function fetchProduct(search = "") {
+    fetch("/api/customer/product" + search, { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+        setStore(data);
+        if (data.length === 0) setProducts(false);
+      })
+      .catch((e) => console.log(e));
+  }
 
   useEffect(() => {
-    if (products.length <= 0)
-      fetch("/api/customer/product", { method: "GET" })
-        .then((res) => res.json())
-        .then((data) => setProducts(data))
-        .catch((e) => console.log(e));
+    if (products.length <= 0) fetchProduct();
   });
 
   async function searchProduct(e) {
     e.preventDefault();
     const search_string = new FormData(e.target).get("search_string");
-    alert(search_string);
+    fetchProduct("?search_string=" + search_string.replaceAll(" ", "+"));
   }
   function sortProduct(e) {
     const { value } = e.target;
-    // console.log(value);
+
+    if (+value === 0) return setProducts(store);
+
+    setProducts([]);
+    const temp = store;
+
+    for (let i = 0; i < temp.length; i++) {
+      for (let j = i; j < temp.length; j++) {
+        if (+value === 1) {
+          if (temp[i].price < temp[j].price) {
+            const t = temp[i];
+            temp[i] = temp[j];
+            temp[j] = t;
+          }
+        }
+        if (+value === -1) {
+          if (temp[i].price > temp[j].price) {
+            const t = temp[i];
+            temp[i] = temp[j];
+            temp[j] = t;
+          }
+        }
+      }
+    }
+
+    setProducts(temp);
   }
 
   return (
-    <div className="w-10/12 m-auto my-5">
+    <div className="w-10/12 m-auto my-5 relative">
       {/* top search and filter line */}
       <div className="w-full flex flex-row flex-wrap justify-between gap-y-1">
         <form onSubmit={searchProduct}>
@@ -81,14 +114,21 @@ function ProductsPage() {
         </span>
       </div>
 
+      {products === false && <center>No Products Found</center>}
+
       {/* products container */}
-      {products.length <= 0 ? (
-        <Loading />
+      {products !== false && products?.length <= 0 ? (
+        // <div className=" bg-red-500 relative w-full h-full">
+        <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
+          <Loading />
+        </span>
       ) : (
+        // </div>
         <div className="w-full my-10 max-lg:mx-10 max-md:mx-5 max-sm:mx-1 grid manager_inventory_product_stock place-items-center gap-y-7">
-          {products?.map((item) => (
-            <ProductSellCard key={item._id} product={item} />
-          ))}
+          {products !== false &&
+            products?.map((item) => (
+              <ProductSellCard key={item._id} product={item} />
+            ))}
         </div>
       )}
     </div>
